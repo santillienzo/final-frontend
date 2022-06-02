b0165b0ede4186cfed6f75f0deee4e877e52fd35
santillienzo <enzo135246@gmail.com>
2022-06-02 15:33:27 -0300

    add banner

--- src/components/features/BannerHome/BannerHome.tsx
+++ src/components/features/BannerHome/BannerHome.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
 import './BannerHome.css'
 
 const BannerHome = () => {
     return (
-        <div className='bannerHome'>BANNER</div>
+        <Carousel style={{width: '95%', margin: 'auto', marginTop: 15}}>
+            <Carousel.Item style={{height: '75vh', }}>
+                <img
+                    style={{objectFit:'contain'}}
+                    className="d-block w-100"
+                    src="https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Primer producto</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item style={{height: '75vh', }}>
+                <img
+                style={{objectFit:'contain'}}
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Segundo producto</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item style={{height: '75vh', }}>
+                <img
+                style={{objectFit:'contain'}}
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Tercer producto</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
