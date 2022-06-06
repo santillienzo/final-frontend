64e7304cb54e8c333547e9da2d36b9135fdbeedd
Emiliano Sinatra <58446200+emisinatra@users.noreply.github.com>
2022-06-06 16:32:51 -0300

    test

--- src/components/layout/Card/Card.tsx
+++ src/components/layout/Card/Card.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 
-let parrillada = require('../../../assets/images/parrillada.jpg');
+interface CardProps {
+    name: string;
+    image: string;
+    description?: string;
+}
 
-function Card() {
+function Card({name, image, description}: CardProps) {
+
     return (
-        <div className='card'>
-            <img src={parrillada} alt="" className="card-img-top"/>
-            <div className='card-body'>
-                <h4 className='card-title'>
-                    Parrillada "Otra vez maldito axel"
-                    <p className="card-text text-secondary">
-                        Especialidad de la casa, con una mezcla de todo.
-                        Matambrito de cerdo al roqueford
-                        Ensalada "Ave Maria purisima de la casa"
-                    </p>
-                </h4>
+        <div className='col-md-4'>
+            <div className='card '>
+                <div style={{display: "flex", justifyContent: "center"}}>
+                    <img src={image} alt="" className="card-img-top" style={{width: "200px", height: "200px"}}/>
+                </div>
+
+                <div className='card-body '>
+                    <h4 className='card-title'>{name}</h4>
+
+                    <div style={{height: "60px",overflowY: "scroll"}}>
+                        <p className="card-text ">{description}</p>
+                    </div>
+
+
+
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <a href="#!" className="btn btn btn-outline-secondary rounded-0 ">
+                            Comprar
+
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
+
     )
 }
 
